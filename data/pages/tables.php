8d078205a5ca7637a2fd3903b22b12cbe1ae1f12
Michael Edwards <medwards@pilgrim.walledcity.ca>
2009-05-24 01:11:30 -0600

    Added scriptaculous and one usage of it

--- data/pages/tables.php
+++ data/pages/tables.php
@@ -1,86 +1,89 @@
-				<h3>tables</h3>
-				<ul>
-					<li>PM press (San Francisco)</li>
-					<li>Sharpie fumes (Halifax)</li>
-					<li>Empowerment (London Ont)</li>
-					<li>Turning the tide (Saskatoon)</li>
-					<li>Bust Out Distro (Calgary)</li>
-					<li>Hammer of Justice (Winnipeg)</li>
-					<li>Junto Library (Winnipeg)</li>
-					<li>Anarchist Black Cross [ABC] (Edmonton and Winnipeg)</li>
-					<li>Anti-Racist Action [ARA] Calgary</li>
-					<li>AK Press (San Francisco)</li>
-					<li>Thought crime ink (Edmonton)</li>
-					<li>Just Say No! Records/Distro</li>
-					<li>Spartacus Books (Vancouver)</li>
-					<li>Black Books (Edmonton)</li>
-					<li>Black Cat Press (Black Cat Press)</li>
-					<li>Haymarket Books (Calgary)</li>
-					<li>Industrial Workers of the World [IWW] (Edmonton GMB)</li>
-					<li>Oaxaca solidarity (Edmonton)</li>
-					<li>Morgan's cupcakes (Edmonton)</li>
-					<li>Revolution Now</li>
-					<li>National Microphone Association (Vancouver)</li>
-					<li>Reinventing Love (Edmonton)</li>
-				</ul>
-				<h3>call for vendors</h3>
-				<p>Since 2002, the Edmonton Anarchist Bookfair has provided Anarchists, activists, and the simply curious the opportunity to learn, share ideas and connect with others. Vendors and speakers from across North America attend each year, with around a thousand attendees passing through during the event. Previous vendors have included AK Press and thoughtcrimeink. Previous speakers have included Ward Churchill and Allan Antliff.</p>
-				<p>The 2008 Edmonton Anarchist Bookfair will again provide a forum to explore ideas and learn. It will be a festival of humanity and liberation, a warming beacon on the prairie. We are inviting vendors, alternative/independent publishers and social justice/radical groups to offer: </p>
-				<ol>
-					<li>Literature - books, pamphlets, zines, etc.</li>
-					<li>Merchandise – clothing, art, crafts, etc.</li>
-					<li>Awareness of and information on your group</li>
-				</ol>
-				<h4>Notes:</h4>
-				<ul>
-					<li>Preference will be given to vendors that provide anarchist/activist literature or merchandise.<li>
-					<li>Limited assistance for travel and billeting may be available for out of town vendors.</li>
-					<li>We are able to accept merchandise from vendors who are unable to attend the event, but only on a limited basis.</li>
-				</ul>
+<h3>tables</h3>
+<a href="#" onclick="new Effect.Fade(this, { queue: 'end' }); new Effect.SlideDown('old-tables', { queue: 'end' }); return false;">
+	Want to see 2008 tables?
+</a>
+<ul id="old-tables" style="display: none;">
+	<h1>Tables in 2008</h1>
+	<li>PM press (San Francisco)</li>
+	<li>Sharpie fumes (Halifax)</li>
+	<li>Empowerment (London Ont)</li>
+	<li>Turning the tide (Saskatoon)</li>
+	<li>Bust Out Distro (Calgary)</li>
+	<li>Hammer of Justice (Winnipeg)</li>
+	<li>Junto Library (Winnipeg)</li>
+	<li>Anarchist Black Cross [ABC] (Edmonton and Winnipeg)</li>
+	<li>Anti-Racist Action [ARA] Calgary</li>
+	<li>AK Press (San Francisco)</li>
+	<li>Thought crime ink (Edmonton)</li>
+	<li>Just Say No! Records/Distro</li>
+	<li>Spartacus Books (Vancouver)</li>
+	<li>Black Books (Edmonton)</li>
+	<li>Black Cat Press (Black Cat Press)</li>
+	<li>Haymarket Books (Calgary)</li>
+	<li>Industrial Workers of the World [IWW] (Edmonton GMB)</li>
+	<li>Oaxaca solidarity (Edmonton)</li>
+	<li>Morgan's cupcakes (Edmonton)</li>
+	<li>Revolution Now</li>
+	<li>National Microphone Association (Vancouver)</li>
+	<li>Reinventing Love (Edmonton)</li>
+</ul>
+<h3>call for vendors</h3>
+<p>Since 2002, the Edmonton Anarchist Bookfair has provided Anarchists, activists, and the simply curious the opportunity to learn, share ideas and connect with others. Vendors and speakers from across North America attend each year, with around a thousand attendees passing through during the event. Previous vendors have included AK Press and thoughtcrimeink. Previous speakers have included Ward Churchill and Allan Antliff.</p>
+<p>The 2008 Edmonton Anarchist Bookfair will again provide a forum to explore ideas and learn. It will be a festival of humanity and liberation, a warming beacon on the prairie. We are inviting vendors, alternative/independent publishers and social justice/radical groups to offer: </p>
+<ol>
+	<li>Literature - books, pamphlets, zines, etc.</li>
+	<li>Merchandise – clothing, art, crafts, etc.</li>
+	<li>Awareness of and information on your group</li>
+</ol>
+<h4>Notes:</h4>
+<ul>
+	<li>Preference will be given to vendors that provide anarchist/activist literature or merchandise.<li>
+	<li>Limited assistance for travel and billeting may be available for out of town vendors.</li>
+	<li>We are able to accept merchandise from vendors who are unable to attend the event, but only on a limited basis.</li>
+</ul>
 
-
-				<h3>Request a Table</h3>
-				<a id="expandform" onclick="document.getElementById('form').style.display = 'block'; document.getElementById('hideform').style.display = 'block'; this.style.display = 'none'; return false;" href="">Click here to fill out the form</a>
-				<a id="hideform" style="display: none;" onclick="document.getElementById('form').style.display = 'none'; document.getElementById('expandform').style.display = 'block'; this.style.display = 'none'; return false;" href="">Click here to hide the form</a>
-				<form id="form" style="display: none" action="mailto:edmontonanarchistbookfair@gmail.com?subject=EABF Table Request" method="post" enctype="text/plain" >
-					<table>
-						<tr><td style="text-align: right">Name:</td><td><input type="text" name="name" /></td></tr>
-						<tr><td style="text-align: right">Affiliation (if any):</td><td><input type="text" name="affiliation" /></td></tr>
-						<tr><td style="text-align: right">Address:</td><td><input type="text" name="address" /></td></tr>
-						<tr><td style="text-align: right">City:</td><td><input type="text" name="city" /></td></tr>
-						<tr><td style="text-align: right">Province/State:</td><td><input type="text" name="province" /></td></tr>
-						<tr><td style="text-align: right">Telephone:</td><td><input type="text" name="phone" /></td></tr>
-						<tr><td style="text-align: right">Fax:</td><td><input type="text" name="fax" /></td></tr>
-						<tr><td style="text-align: right"><b style="color: red">Email:</b></td><td><input type="text" name="email" /></td></tr>
-						<tr><td style="text-align: right">Date:</td><td><input type="text" name="date" /></td></tr>
-					</table>
-					<table style="width: 100%">
-						<tr><td>How many tables will you require?</td><td><input type="text" name="tables_required" /></td></tr>
-						<tr><td colspan="2">Brief description of what your table(s) will offer the public:</td></tr>
-						<tr><td colspan="2"><textarea name="description" style="width: 100%"></textarea></td></tr>
-					</table>
-					<h4>Travel Information</h4>
-					<table style="width: 100%">
-						<tr><td>1. Where will you (and your co-presenters) be traveling from, and by what method?</td></tr>
-						<tr><td><textarea name="travelling_from" style="width: 100%"></textarea></td></tr>
-						<tr><td>2. If you will be driving, can you drive other people that need rides in your area? If so, how many spaces can you provide?</td></tr>
-						<tr><td><textarea name="rides_offered" style="width: 100%"></textarea></td></tr>
-						<tr><td>3. If you are not driving, can you ride with other people? How many spaces will you need?</td></tr>
-						<tr><td><textarea name="rides_needed" style="width: 100%"></textarea></td></tr>
-						<tr><td>4. Do you require a travel subsidy? If so, how much?</td></tr>
-						<tr><td><textarea name="travel_subsidy" style="width: 100%"></textarea></td></tr>
-					</table>
-					<h4>Billeting:</h4>
-					<table style="width: 100%">
-						<tr><td>1. Will you need a place to stay while you are in Edmonton? If so, for how many, and for how long? Please provide dates.</td></tr>
-						<tr><td><textarea name="billeting_required" style="width: 100%"></textarea></td></tr>
-						<tr><td>2. Other concerns or considerations, including smoke or pet allergies?</td></tr>
-						<tr><td><textarea name="billeting_needs" style="width: 100%"></textarea></td></tr>
-					</table>
-					<h4>Childcare:</h4>
-					<table style="width: 100%">				
-						<tr><td>Will you require childcare? For how many children?</td></tr>
-						<tr><td><textarea name="childcare" style="width: 100%"></textarea></td></tr>
-					</table>
-					<input type="submit" name="submit" value="Submit" onclick="if(document.getElementById('form').email.value.length == 0 || document.getElementById('form').email.value == null) { alert('Please fill out your email'); return false; }" /> or <a href="mailto:edmontonanarchistbookfair@gmail.com">email us</a>
-				</form>
+<h3>Request a Table</h3>
+<a id="expandform" onclick="document.getElementById('form').style.display = 'block'; document.getElementById('hideform').style.display = 'block'; this.style.display = 'none'; return false;" href="">Click here to fill out the form</a>
+<a id="hideform" style="display: none;" onclick="document.getElementById('form').style.display = 'none'; document.getElementById('expandform').style.display = 'block'; this.style.display = 'none'; return false;" href="">Click here to hide the form</a>
+<form id="form" style="display: none" action="mailto:edmontonanarchistbookfair@gmail.com?subject=EABF Table Request" method="post" enctype="text/plain" >
+	<table>
+		<tr><td style="text-align: right">Name:</td><td><input type="text" name="name" /></td></tr>
+		<tr><td style="text-align: right">Affiliation (if any):</td><td><input type="text" name="affiliation" /></td></tr>
+		<tr><td style="text-align: right">Address:</td><td><input type="text" name="address" /></td></tr>
+		<tr><td style="text-align: right">City:</td><td><input type="text" name="city" /></td></tr>
+		<tr><td style="text-align: right">Province/State:</td><td><input type="text" name="province" /></td></tr>
+		<tr><td style="text-align: right">Telephone:</td><td><input type="text" name="phone" /></td></tr>
+		<tr><td style="text-align: right">Fax:</td><td><input type="text" name="fax" /></td></tr>
+		<tr><td style="text-align: right"><b style="color: red">Email:</b></td><td><input type="text" name="email" /></td></tr>
+		<tr><td style="text-align: right">Date:</td><td><input type="text" name="date" /></td></tr>
+	</table>
+	<table style="width: 100%">
+		<tr><td>How many tables will you require?</td><td><input type="text" name="tables_required" /></td></tr>
+		<tr><td colspan="2">Brief description of what your table(s) will offer the public:</td></tr>
+		<tr><td colspan="2"><textarea name="description" style="width: 100%"></textarea></td></tr>
+	</table>
+	<h4>Travel Information</h4>
+	<table style="width: 100%">
+		<tr><td>1. Where will you (and your co-presenters) be traveling from, and by what method?</td></tr>
+		<tr><td><textarea name="travelling_from" style="width: 100%"></textarea></td></tr>
+		<tr><td>2. If you will be driving, can you drive other people that need rides in your area? If so, how many spaces can you provide?</td></tr>
+		<tr><td><textarea name="rides_offered" style="width: 100%"></textarea></td></tr>
+		<tr><td>3. If you are not driving, can you ride with other people? How many spaces will you need?</td></tr>
+		<tr><td><textarea name="rides_needed" style="width: 100%"></textarea></td></tr>
+		<tr><td>4. Do you require a travel subsidy? If so, how much?</td></tr>
+		<tr><td><textarea name="travel_subsidy" style="width: 100%"></textarea></td></tr>
+	</table>
+	<h4>Billeting:</h4>
+	<table style="width: 100%">
+		<tr><td>1. Will you need a place to stay while you are in Edmonton? If so, for how many, and for how long? Please provide dates.</td></tr>
+		<tr><td><textarea name="billeting_required" style="width: 100%"></textarea></td></tr>
+		<tr><td>2. Other concerns or considerations, including smoke or pet allergies?</td></tr>
+		<tr><td><textarea name="billeting_needs" style="width: 100%"></textarea></td></tr>
+	</table>
+	<h4>Childcare:</h4>
+	<table style="width: 100%">				
+		<tr><td>Will you require childcare? For how many children?</td></tr>
+		<tr><td><textarea name="childcare" style="width: 100%"></textarea></td></tr>
+	</table>
+	<input type="submit" name="submit" value="Submit" onclick="if(document.getElementById('form').email.value.length == 0 || document.getElementById('form').email.value == null) { alert('Please fill out your email'); return false; }" /> or <a href="mailto:edmontonanarchistbookfair@gmail.com">email us</a>
+</form>
